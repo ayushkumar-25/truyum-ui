d1a4bc207faf8fc1f6f74b4d4201ed7695180453
Ayush Kumar <ayush2608@gmail.com>
2021-06-21 11:26:16 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,34 @@
 function checkValidation() {
-    var title = document.getElementById("title").value;
-    var price = document.getElementById("price").value;
+    var title = document.getElementById("title").value.trim();
+    var price = document.getElementById("price").value.trim();
     var dateOfLaunch = document.getElementById("dateOfLaunch").value;
 
+    // validation check for title-name
     if (title == "") {
         alert("Please enter Title.");
+        return false;
     }
+    if (!(title.length > 1 && title.length < 66)) {
+        alert("Title should have 2 to 65 characters!");
+        return false;
+    }
+
+    // validation check for price
     if (price == "") {
         alert("Please enter Price.");
+        return false;
     }
-    else { location.href = "edit-menu-item-status.html"; }
+    if (!price.match(/^\d+$/)) {
+        alert("Price has to be a number.");
+        return false;
+    }
 
+    // validation check for date-of-launch
+    if (dateOfLaunch == "") {
+        alert("Date is required");
+        return false;
+    }
+
+    else { location.href = "edit-menu-item-status.html"; } //successfully saved message
     return false;
 }
