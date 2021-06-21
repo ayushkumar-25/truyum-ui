faa5c17e0c24ed855dbea96fbb820b6d6473c6d8
Ayush Kumar <ayush2608@gmail.com>
2021-06-21 10:12:24 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
-function checkValidation(){
-    location.href = "edit-menu-item-status.html";
+function checkValidation() {
+    var title = document.getElementById("title").value;
+    var price = document.getElementById("price").value;
+    var dateOfLaunch = document.getElementById("dateOfLaunch").value;
+
+    if (price == "") {
+        alert("Please enter Price.");
+    }
+    else { location.href = "edit-menu-item-status.html"; }
+
     return false;
 }
